ad6c82bd9faeff74b36ca91912dd4e18fdb075ab
d3lap1ace <reald3lap1ace@gmail.com>
2025-08-23 23:12:45 +0800

    add gitso.rb

---
+++ Formula/gitso.rb
@@ -0,0 +1,37 @@
+class Gitso < Formula
+  desc      "Clone GitHub repo into ~/Documents/GitHub.com/<owner>/<repo>"
+  homepage  "https://github.com/d3Lap1ace/gitso"
+  url       "https://github.com/d3Lap1ace/gitso/archive/refs/tags/v0.1.3.tar.gz"
+  sha256    "PUT_SOURCE_TARBALL_SHA256_HERE"
+  license   "MIT"
+
+  livecheck do
+    url :stable          # check the same tag source used by :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    # 与 goreleaser 的 ldflags 保持一致
+    commit = Utils.git_short_head(length: 7)
+    date    = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    ldflags = %W[
+      -s -w
+      -X gitso/cmd.version=#{version}
+      -X gitso/cmd.commit=#{commit}
+      -X gitso/cmd.date=#{date}
+    ]
+
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args(
+      ldflags:  ldflags,
+      output:   bin/"gitso",
+    ), "./cmd/gitso"
+  end
+
+  test do
+    assert_match "Clone GitHub repo", shell_output("#{bin}/gitso --help")
+  end
+end
